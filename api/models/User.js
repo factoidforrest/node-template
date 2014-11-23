38a048473edc7028de1486d8293419db18db2842
forrest <light24bulbs@gmail.com>
2014-11-23 18:25:05 -0500

    merged the usre model from master

---
+++ api/models/User.js
@@ -0,0 +1,30 @@
+
+
+bcrypt = require("bcrypt")
+
+module.exports = {
+
+  attributes: {
+  	
+
+  	token: 'string',
+    tokenSecret: 'string',
+    password: 'string'
+    
+  },
+
+  beforeCreate: function (attrs, next) {
+  	console.log('user before create called')
+  	/*
+	  bcrypt = require("bcrypt")
+	  bcrypt.genSalt 10, (err, salt) ->
+	    return next(err) if err
+	    bcrypt.hash attrs.password, salt, (err, hash) ->
+	      return next(err) if err
+	      attrs.password = hash
+	      next()
+	   */
+	 }
+
+
+}
