f143991977dcf664e292c5a278f962b785e24e0d
forrest <light24bulbs@gmail.com>
2014-11-29 17:09:50 -0800

    sending parameters

--- assets/scripts/controllers/register.js
+++ assets/scripts/controllers/register.js
@@ -4,12 +4,35 @@
 
 
 angular.module('mobileGiftCardWebApp')
-    .controller('RegisterCtrl',["$scope", "UserService", "ENV", function ($scope, userService, ENV) {
+    .controller('RegisterCtrl',["$scope", "$http", "UserService", "ENV", function ($scope, $http, userService, ENV) {
+  		$scope.formData = {};
+      $scope.apiRoot = ENV.apiRoot;
+      $scope.submit = function () {
+      	console.log('submit called with formdata')
+      	console.log($scope.formData)
+      	$http({
+		        method  : 'POST',
+		        url     : 'auth/register',
+		        data    : $.param($scope.formData),  // pass in data as strings
+		        headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
+		    })
+        .success(function(data) {
+          console.log('server responded with data: ', data);
 
-        $scope.apiRoot = ENV.apiRoot;
+          if (!data.success) {
+          	// if not successful, bind errors to error variables
+              $scope.errorUsernmae = data.errors.username;
+              $scope.errorPassord = data.errors.password;
+              $scope.errorConfirm = data.errors.confirm;
+          } else {
+          	// if successful, bind success message to message
+            $scope.message = data.message;
+          }
+        });
 
-        $scope.awesomeThings = [
+		  };
+      $scope.awesomeThings = [
 
-		    ];
+	    ];
     }]);
 
