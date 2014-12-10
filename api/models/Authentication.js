6110f99a2008773b5b72916ca296c41c6fad7516
forrest <light24bulbs@gmail.com>
2014-12-10 19:23:08 +0100

    seperating authentications and users bit by bit.  models more or less done. Leaving password in the user model for simplicity

--- api/models/Authentication.js
+++ api/models/Authentication.js
@@ -8,40 +8,12 @@ module.exports = {
 
   	token: 'string',
     tokenSecret: 'string',
-    password: 'string',
     email: 'string',
 
-    validPassword: function(password) {
-    	console.log('checking password')
-    	//would be more efficient to do this async, can figure that out once load becomes a problem
-    	valid = bcrypt.compareSync(password, this.password)
-    	console.log('correct password? : ', valid)
-    	console.log('the password is', password)
-    	return valid;
-    }
-    
   }
   
   , beforeCreate: function (attrs, next) {
-
-  	console.log('auth before create called and the attributes are')
-  	console.log(attrs)
-	  //bcrypt = require("bcrypt")
-	  User.findByEmail(attrs.email).done(function(err, user){
-	  	console.log('found users matching email: ', user)
-	  });
-	  if (attrs.strategy === 'local'){
-
-	    bcrypt.hash(attrs.password, null, null, function (err, hash) {
-	      if (err) return next(err);
-	      attrs.password = hash;
-	    	//delete attrs.passwordConfirmation;
-	      next();
-	    });
-
-		} else {
-			next();
-	 	}
+		next();
 	}
 	,afterCreate: function(attrs, next){
 		//console.log('aftercreate callback called with attrs: ', attrs)
@@ -50,3 +22,4 @@ module.exports = {
 	 
 
 }
+
--- api/models/User.js
+++ api/models/User.js
@@ -10,13 +10,47 @@ module.exports = {
   	token: 'string',
     tokenSecret: 'string',
     password: 'string',
-    email: 'string'
+    email: 'string',
+
+    validPassword: function(password) {
+      console.log('checking password')
+      //would be more efficient to do this async, can figure that out once load becomes a problem
+      valid = bcrypt.compareSync(password, this.password)
+      console.log('correct password? : ', valid)
+      console.log('the password is', password)
+      return valid;
+    },
+    setPassword: function(password, next){
+      var self = this;
+      bcrypt.hash(self.attrs.password, null, null, function (err, hash) {
+        if (err) return next(err);
+        self.attrs.password = hash;
+        next();
+      });
+    }
     
   }
   
-  ,beforeCreate: function (attrs, next) {
-		next();
-	}
+  , beforeCreate: function (attrs, next) {
+
+    console.log('auth before create called and the attributes are')
+    console.log(attrs)
+    //bcrypt = require("bcrypt")
+    //User.findByEmail(attrs.email).done(function(err, user){
+    //  console.log('found users matching email: ', user)
+    //});
+    if (typeof(attrs.password) !== 'undefined'){//(attrs.strategy === 'local'){
+      console.log("converting password to hash")
+      bcrypt.hash(attrs.password, null, null, function (err, hash) {
+        if (err) return next(err);
+        attrs.password = hash;
+        next();
+      });
+
+    } else {
+      next();
+    }
+  }
 	,afterCreate: function(attrs, next){
 		next();
 	}
