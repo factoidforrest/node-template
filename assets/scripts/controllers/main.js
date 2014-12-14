c39b2da269552fc16f163f9813070ce45472862e
forrest <light24bulbs@gmail.com>
2014-12-14 14:13:08 +0100

    added login warning, about to switch login form to angular

--- assets/scripts/controllers/main.js
+++ assets/scripts/controllers/main.js
@@ -7,9 +7,10 @@
  * Controller of the mobileGiftCardWebApp
  */
 angular.module('mobileGiftCardWebApp')
-  .controller('MainCtrl',["$scope", "$rootScope", "UserService", "ENV", function ($scope, $rootScope, userService, ENV) {
+  .controller('MainCtrl',["$location", "$scope", "$rootScope", "UserService", "ENV", function ($location, $scope, $rootScope, userService, ENV) {
         'use strict';
-
+        console.log('the hash is ', $location.hash())
+        $scope.error = window.location.hash.error;
         $scope.apiRoot = ENV.apiRoot;
 
   }]);
